3ad4da2e9f6c959b69aabbd19838aa4345b58842
tyousk <tyou22211@gmail.com>
2021-09-21 09:27:05 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,11 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def new
-  end
+  # def new
+  # end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 end
